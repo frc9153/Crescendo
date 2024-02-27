4f6b3615cf63b7361659abe04ba3e5ca29c4b464
onesome <onesomegit@outlook.com>
2024-02-27 17:09:52 -0600

    It's not a subsystem like at all

--- src/main/java/frc/robot/subsystems/DriveSwerve.java
+++ src/main/java/frc/robot/subsystems/DriveSwerve.java
@@ -16,6 +16,7 @@ import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
+import frc.robot.utils.MAXSwerveModule;
 import frc.robot.utils.Vector2;
 
 public class DriveSwerve extends SubsystemBase {
